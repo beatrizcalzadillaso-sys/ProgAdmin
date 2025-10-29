817eb33d93e612a7ff77311c702a54be9f15c59a
shadowlebronjames <julenalejandro.perezac@elorrieta-errekamari.com>
2025-10-29 19:57:51 +0100

    "Completar" el java de productos, por refactorizar

--- productos.java
+++ productos.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class productos {
 	
+	// Siento que tengan que leer esta pieza de codigo. (┬┬﹏┬┬)
+	
 	static Scanner sc = new Scanner(System.in);
 	public static productoItem listaProductos[] = new productoItem[50];
 	
@@ -29,6 +31,7 @@ public class productos {
 		return p;
 	}
 
+	//PLACEHOLDER, CAMBIAR A OTRA CLASE CUANDO EXISTA
 	public static void inicializarListaProductos() {
 
 		listaProductos[0] = new productoItem(11234, 1, "Agua natural", 1.26);
@@ -39,8 +42,9 @@ public class productos {
 
 	public static void main(String[] args) {
 		
+		//PLACEHOLDER
 		inicializarListaProductos();
-		
+		//TEST
 		listaProductos[4] = (new productoItem(15678, 1, "Zumo de piña", 1.35));
 		listaProductos[5] = (new productoItem(21789, 2, "Kombucha", 2.07));
 		listaProductos[6] = (new productoItem(22456, 2, "Kombucha", 2.07));
@@ -96,17 +100,25 @@ public class productos {
 		
 		int op = buscarNull();
 		
-		System.out.print("ID: ");
-		int id = Integer.valueOf(sc.nextLine());
-		
-		System.out.print("Tipo: ");
-		int ti = Integer.valueOf(sc.nextLine());
+		int id = 0;
+		int ti = 0;
+		do {
+			System.out.print("ID: ");
+			id = Integer.valueOf(sc.nextLine());
+			do {
+				System.out.print("Tipo: ");
+				ti = Integer.valueOf(sc.nextLine());
+			} while(!validarTipoProd(ti));
+		} while(!validarCodProd(id, ti));
 		
 		System.out.print("nomber: ");
 		String no = sc.nextLine();
 		
-		System.out.print("Precio: ");
-		double pr = Double.valueOf(sc.nextLine());
+		double pr = 0.0;
+		do {
+			System.out.print("Precio: ");
+			pr = Double.valueOf(sc.nextLine());
+		} while(!validarPrecio(pr));
 		
 		listaProductos[op] = (new productoItem(id, ti, no, pr));
 	}
@@ -116,14 +128,74 @@ public class productos {
 		int op = Integer.valueOf(sc.nextLine());
 		
 		int mo = buscar(op);
+		int opp = 0;
 		
-		System.out.println("Tipo (previamente "+listaProductos[mo].getTipoNum()+"):");
-		listaProductos[mo].setTipoNum(Integer.valueOf(sc.nextLine()));
+		do {
+			System.out.println("Tipo (previamente "+listaProductos[mo].getTipoNum()+"):");
+			opp = Integer.valueOf(sc.nextLine());
+		} while(!validarTipoProd(opp));
+		listaProductos[mo].setTipoNum(opp);
 		
 		System.out.print("nomber (previamente "+listaProductos[mo].getNomProd()+"): ");
 		listaProductos[mo].setNomProd(sc.nextLine());
 		
-		System.out.print("Precio (previamente "+listaProductos[mo].getPrecio()+"): ");
-		listaProductos[mo].setPrecio(Double.valueOf(sc.nextLine()));
+		double oppp = 0;
+		do {
+			System.out.print("Precio (previamente "+listaProductos[mo].getPrecio()+"): ");
+			oppp = Double.valueOf(sc.nextLine());
+		} while(!validarPrecio(oppp));
+		listaProductos[mo].setPrecio(oppp);
+	}
+	
+	private static boolean validarTipoProd(int ti) {
+		boolean jesse = false;
+		
+		if(ti>0&&ti<5) {
+			jesse = true;
+		}
+		else {
+			System.out.println("Tipo incorrecto, vuelve a intentarlo.");
+		}
+		
+		return jesse;
+	}
+	
+	private static boolean validarCodProd(int cod, int ti) {
+		boolean walter = false;
+		int codbo2 = Integer.parseInt(String.valueOf(cod).substring(0,1));
+		
+		if(String.valueOf(cod).length() == 5) {
+			if(codbo2 == ti) {
+				walter = true;
+			}
+			else {
+				System.out.println("El código de producto no se corresponde con los tipos de productos existentes");
+			}
+		}
+		else {
+			System.out.println("El código de producto no se corresponde con la cantidad de cifras");
+		}
+		
+		return walter;
+	}
+	
+	private static boolean validarPrecio(double pr) {
+		boolean saul = true;
+		
+		double pr1 = pr * 100;
+		int goodman = String.valueOf(pr1).length();
+		int pr2 = Integer.parseInt(String.valueOf(pr).substring(0,1));
+		
+		if (pr2==0) {
+			saul = false;
+		}
+		if(goodman>5) {
+			saul = false;
+		}
+		if(!saul) {
+			System.out.println("El precio excede/está por debajo el rango establecido, rectifique el precio");
+		}
+		
+		return saul;
 	}
 }
